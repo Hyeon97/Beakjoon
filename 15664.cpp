acfacb4209010a6a49d4ff98fd1f33cbdfaaef8a
배크준 <Hyeon97>
2020-08-22 01:20:21 +0900

    N과 M (10) <백트래킹>

---
+++ 15664.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int out[9];
+bool v[9];
+
+void DFS(int cnt, int idx) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << out[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	//직전에 사용한 숫자 저장
+	int temp = -1;
+
+	for (int i = idx; i < n; i++) {
+		//비 내림차순 조건
+		if (!v[i] && temp != arr[i] && out[cnt - 1] <= arr[i]) {
+			v[i] = true;
+			out[cnt] = arr[i];
+			temp = arr[i];
+			DFS(cnt + 1, idx + 1);
+			v[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	DFS(0, 0);
+
+	return 0;
+}
