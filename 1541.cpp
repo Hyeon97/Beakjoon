b44dc319467363d9e6615fe491ac62a484b32a22
배크준 <Hyeon97>
2020-09-04 16:16:32 +0900

    잃어버린 괄호 <문자열처리>

---
+++ 1541.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string S;
+
+int P(int idx, int op) {
+	string temp;
+	for (int i = op == 0 ? op : op + 1; i < idx; i++) {
+		temp += S[i];
+	}
+	int sum = 0;
+	for (int i = temp.length() - 1, j = 1; i >= 0; i--, j *= 10) {
+		sum += (temp[i]-'0') * j;
+	}
+	return sum;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> S;
+	int result = 0;
+	for (int i = 0; i <= S.length(); i++) {
+		if ('0' <= S[i] && S[i] <= '9') {
+			string temp;
+			for (int j = i; ; j++) {
+				if (S[j] == '+' || S[j] == '-' || S[j] == '\0') {
+					i = (j - 1);
+					int tempsum = 0;
+					for (int k = temp.length() - 1, p = 1; k >= 0; k--, p *= 10) {
+						tempsum += (temp[k]-'0') * p;
+					}
+					result += tempsum;
+					break;
+				}
+				temp += S[j];
+			}
+		}
+		else if (S[i] == '+') {
+			string temp;
+			for (int j = i+1; ; j++) {
+				if (S[j] == '+' || S[j] == '-' || S[j] == '\0') {
+					i = (j - 1);
+					int tempsum = 0;
+					for (int k = temp.length() - 1, p = 1; k >= 0; k--, p *= 10) {
+						tempsum += (temp[k] - '0') * p;
+					}
+					result += tempsum;
+					break;
+				}
+				temp += S[j];
+			}
+		}
+		else if (S[i] == '-') {
+			string temp;
+			int tempsum = 0;
+			for (int j = i+1; ; j++) {
+				if (S[j] == '-' || S[j] == '\0') {
+					i = (j - 1);
+					for (int k = temp.length() - 1, p = 1; k >= 0; k--, p *= 10) {
+						tempsum += (temp[k] - '0') * p;
+					}
+					result -= tempsum;
+					break;
+				}
+				else if (S[j] == '+') {
+					for (int k = temp.length() - 1, p = 1; k >= 0; k--, p *= 10) {
+						tempsum += (temp[k] - '0') * p;
+					}
+					temp = "";
+				}
+				else temp += S[j];
+			}
+		}
+
+	}
+	cout << result;
+
+
+	return 0;
+}
+
