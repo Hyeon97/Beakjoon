81b68aeb99062ad89d1f239b00d219040ab44955
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2021-07-25 16:08:04 +0900

    N_Queen

--- 9663.cpp
+++ 9663.cpp
@@ -3,29 +3,32 @@
 
 using namespace std;
 
+int N, ans;
 int map[15];
-int n, result = 0;
 
-bool check(int num) {
+bool ch(int num) {
 	for (int i = 0; i < num; i++) {
 		if (map[i] == map[num] || abs(map[num] - map[i]) == (num - i))return false;
 	}
 	return true;
 }
 
-void N_Qeen(int num) {
-	if (num == n)result += 1;
-	else {
-		for (int i = 0; i < n; i++) {
-			map[num] = i;
-			if (check(num))N_Qeen(num + 1);
-		}
+void NQ(int cnt) {
+	if (cnt == N) {
+		ans++;
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		map[cnt] = i;
+		if (ch(cnt))NQ(cnt + 1);
 	}
 }
 
+
 int main() {
-	cin >> n;
-	N_Qeen(0);
-	cout << result;
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	NQ(0);
+	cout << ans;
 	return 0;
 }
